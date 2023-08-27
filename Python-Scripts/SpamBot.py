6115a6910dd2d5e62dde527856bc0c145707fdcf
4NGE1 <136937878+QxAngel@users.noreply.github.com>
2023-08-27 13:22:18 -0700

    Update SpamBot.py

--- Python-Scripts/SpamBot.py
+++ Python-Scripts/SpamBot.py
@@ -1,15 +1,21 @@
+ # author : Angel - @qx4ngel ,     version-1.0
+
+ #VISITA MI GITHUB PARA MAS ↓
+ #https://github.com/QxAngel
+
+
 # en la terminal escribe "pip install PyAutoGUI" para instalar (NECESARIO)
 import pyautogui as pg 
 
 import time                    
 
-# el tiempo que tarda en empezar a spamear recomiendo dejarlo en "5"
+# el tiempoen el que inicia a spamear
 time.sleep(5)
 
 # cambiar por el numero de veces que quieres enviar el mensaje
 for i in range(6):
 
 # Remplaza por el mensaje que deseas spamear
-    pg.write("Spam Bot Hecho Por @6ky_l")
+    pg.write("Spam Bot Hecho Por @qx4ngel")
     pg.press("enter")
     
