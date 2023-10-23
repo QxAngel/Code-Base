6cc935580817f4698c09a8ff843742077c0aa7a6
QxAngel <136937878+QxAngel@users.noreply.github.com>
2023-10-22 22:10:53 -0700

    Update CalcScripter.py

--- Python-Scripts/CalcScripter.py
+++ Python-Scripts/CalcScripter.py
@@ -3,7 +3,6 @@
  #VISITA MI GITHUB PARA MAS ↓
  #https://github.com/QxAngel
 
-
 import math
 
 def suma(a, b):
@@ -30,33 +29,41 @@ def raiz_cuadrada(a):
     else:
         return "No se puede calcular la raíz de un número negativo"
 
-print("CalcScripter By Angel")
-print("1. Suma")
-print("2. Resta")
-print("3. Multiplicación")
-print("4. División")
-print("5. Cuadrado")
-print("6. Raíz Cuadrada")
+while True:
+    print("CalcScripter By Angel")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Cuadrado")
+    print("6. Raíz Cuadrada")
+    print("Presione 'r' para salir")
 
-opcion = int(input("Seleccione una operación (1/2/3/4/5/6): "))
+    opcion = input("Seleccione una operación (1/2/3/4/5/6/r): ")
 
-num1 = float(input("Ingrese el primer número: "))
+    if opcion == 'r':
+        break  # Salir del bucle si se ingresa 'r'
+    
+    if opcion in ['1', '2', '3', '4', '5', '6']:
+        num1 = float(input("Ingrese el primer número: "))
 
-if opcion in [1, 2, 3, 4]:
-    num2 = float(input("Ingrese el segundo número: "))
-    if opcion == 1:
-        resultado = suma(num1, num2)
-    elif opcion == 2:
-        resultado = resta(num1, num2)
-    elif opcion == 3:
-        resultado = multiplicacion(num1, num2)
-    elif opcion == 4:
-        resultado = division(num1, num2)
-elif opcion == 5:
-    resultado = cuadrado(num1)
-elif opcion == 6:
-    resultado = raiz_cuadrada(num1)
-else:
-    resultado = "Opción no válida"
-
-print("Resultado:", resultado)
+        if opcion in ['1', '2', '3', '4']:
+            num2 = float(input("Ingrese el segundo número: "))
+            if opcion == '1':
+                resultado = suma(num1, num2)
+            elif opcion == '2':
+                resultado = resta(num1, num2)
+            elif opcion == '3':
+                resultado = multiplicacion(num1, num2)
+            elif opcion == '4':
+                resultado = division(num1, num2)
+        elif opcion == '5':
+            resultado = cuadrado(num1)
+        elif opcion == '6':
+            resultado = raiz_cuadrada(num1)
+        else:
+            resultado = "Opción no válida"
+        
+        print("Resultado:", resultado)
+    else:
+        print("Opción no válida")
